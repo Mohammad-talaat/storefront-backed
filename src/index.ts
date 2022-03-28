22ee5736ad39fb346a2c0bbf61c3bb9b57f6fc67
Muhammadtalaat <eng.muhamedtalaat@gmail.com>
2022-03-28 20:53:32 +0200

    adding jasmine to the project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
 
+import express,{Application} from 'express'
+
+const app:Application = express()
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome to our app</h1>')
+})
+
+
+app.listen(3000,()=>{
+    console.log('port is running on port 3000')
+})
+
+export default app;
